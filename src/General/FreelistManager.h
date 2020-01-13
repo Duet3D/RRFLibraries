6bfefcced2ea5ffa2f7693399f5a5e76e6842ba1
David Crocker <dcrocker@eschertech.com>
2020-01-13 16:22:42 +0000

    Turned FreelistManager into a namespace to avoid name clashes in RRF

--- src/General/FreelistManager.h
+++ src/General/FreelistManager.h
@@ -13,57 +13,60 @@
 #include <cstddef>
 #include "../RTOSIface/RTOSIface.h"
 
-// Free list manager
-template<size_t Sz> class FreelistManager
+namespace FreelistManager
 {
-public:
-	static void *Allocate() noexcept;
-	static void Release(void *p) noexcept;
-
-private:
-	static void *freelist;
-};
-
-template<size_t Sz> void *FreelistManager<Sz>::freelist = nullptr;
-
-template<size_t Sz> void *FreelistManager<Sz>::Allocate() noexcept
-{
-	TaskCriticalSectionLocker lock;
-
-	if (freelist != nullptr)
+	// Free list manager class
+	template<size_t Sz> class Freelist
 	{
-		void * const p = freelist;
-		freelist = *static_cast<void **>(p);
-		return p;
+	public:
+		static void *Allocate() noexcept;
+		static void Release(void *p) noexcept;
+
+	private:
+		static void *freelist;
+	};
+
+	template<size_t Sz> void *Freelist<Sz>::freelist = nullptr;
+
+	template<size_t Sz> void *Freelist<Sz>::Allocate() noexcept
+	{
+		TaskCriticalSectionLocker lock;
+
+		if (freelist != nullptr)
+		{
+			void * const p = freelist;
+			freelist = *static_cast<void **>(p);
+			return p;
+		}
+		return ::operator new(Sz);
 	}
-	return ::operator new(Sz);
-}
 
-template<size_t Sz> void FreelistManager<Sz>::Release(void *p) noexcept
-{
-	TaskCriticalSectionLocker lock;
+	template<size_t Sz> void Freelist<Sz>::Release(void *p) noexcept
+	{
+		TaskCriticalSectionLocker lock;
 
-	*static_cast<void **>(p) = freelist;
-	freelist = p;
-}
+		*static_cast<void **>(p) = freelist;
+		freelist = p;
+	}
 
-// Macro to return the size of objects of a given type rounded up to a multiple of 8 bytes.
-// We use this to reduce the number of freelists that we need to keep.
-inline constexpr size_t RoundedUpSize(size_t rawSize)
-{
-	constexpr size_t sizeIncrement = 8;
-	return ((rawSize + (sizeIncrement - 1u)) & ~(sizeIncrement - 1u));
-}
+	// Macro to return the size of objects of a given type rounded up to a multiple of 8 bytes.
+	// We use this to reduce the number of freelists that we need to keep.
+	inline constexpr size_t RoundedUpSize(size_t rawSize)
+	{
+		constexpr size_t sizeIncrement = 8;
+		return ((rawSize + (sizeIncrement - 1u)) & ~(sizeIncrement - 1u));
+	}
 
-// Operators new and delete for the classes that we want to use these freelists for should call the following functions
-template<class T> inline void *Allocate() noexcept
-{
-	return FreelistManager<RoundedUpSize(sizeof(T))>::Allocate();
-}
+	// Operators new and delete for the classes that we want to use these freelists for should call the following functions
+	template<class T> inline void *Allocate() noexcept
+	{
+		return Freelist<RoundedUpSize(sizeof(T))>::Allocate();
+	}
 
-template<class T> inline void Release(void *p) noexcept
-{
-	FreelistManager<RoundedUpSize(sizeof(T))>::Release(p);
+	template<class T> inline void Release(void *p) noexcept
+	{
+		Freelist<RoundedUpSize(sizeof(T))>::Release(p);
+	}
 }
 
 #endif /* SRC_LIBRARIES_GENERAL_FREELISTMANAGER_H_ */
