a31c37f6ce61fb89ee8689a71e035d9b43a81668
David Crocker <dcrocker@eschertech.com>
2020-12-13 19:15:58 +0000

    Added a comment

--- src/General/SafeVsnprintf.h
+++ src/General/SafeVsnprintf.h
@@ -16,6 +16,7 @@
 // The putc function must behave as follows:
 // If the character passed is not zero: if possible, send or store the character and return true; else store a terminator if necessary and return false to terminate the vuprintf call.
 // If the character passed is zero, this signals the last character being printed at this time, so store a terminator if necessary. The return value is unimportant.
+// NOTE: whenever a format specifier of exactly "%.s" is encountered, the string argument is JSON-escaped.
 int vuprintf(std::function<bool(char) /*noexcept*/ > putc, const char *format, va_list args) noexcept;
 int uprintf(std::function<bool(char) /*noexcept*/ > putc, const char *format, ...) noexcept __attribute__ ((format (printf, 2, 3)));
 
