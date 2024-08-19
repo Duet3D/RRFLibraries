3c12cfee4a450057339c3fbb2d2b1616dba9be13
David Crocker <dcrocker@eschertech.com>
2024-08-19 12:33:51 +0100

    Added function WriteLockedPointer::Release()

--- src/RTOSIface/RTOSIface.h
+++ src/RTOSIface/RTOSIface.h
@@ -528,6 +528,8 @@ public:
 	T& operator*() const noexcept { return *ptr; }
 	T* Ptr() const noexcept { return ptr; }
 
+	void Release() noexcept { locker.Release(); }
+
 	WriteLockedPointer(const WriteLockedPointer<T>&) = delete;
 	WriteLockedPointer<T>& operator=(const WriteLockedPointer<T>&) = delete;
 
