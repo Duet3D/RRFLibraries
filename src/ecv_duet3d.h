7900b440a964f14286b94b7a72491d852188a7f5
David Crocker <dcrocker@eschertech.com>
2024-12-26 10:40:49 +0000

    Added typedef for null terminated C string

--- src/ecv_duet3d.h
+++ src/ecv_duet3d.h
@@ -5,7 +5,6 @@
  *      Author: David
  *
  *  This is the file we include instead of including ecv.h directly.
- *  It includes the standard ecv.h and then #undefs those macros which cause problems for C++ standard include files.
  */
 
 #ifndef SRC_GENERAL_ECV_RRF_H_
@@ -13,6 +12,7 @@
 
 #include "ecv_original.h"
 
+// Undefine eCv macros that are used in some C++ headers. Use the versions prefixed with "_ecv_" for these instead.
 #undef array	// used in C++ standard headers
 #undef out
 #undef value
@@ -22,7 +22,13 @@
 // C++ doesn't define a 16-bit floating point type, so eCv uses _ecv_float16_t
 
 #ifdef __ECV__
-typedef _ecv_float16_t __fp16;		// define ARM GCC's floating type to be the same as the eCv builtin one
+
+// RRF typedefs float16_t to be ARM's __fp16. So define __fp16 to be the same type as the eCv builtin one.
+typedef _ecv_float16_t __fp16;
+
 #endif
+
+// Define a type name to mean a C string that is guaranteed to be null terminated
+typedef const char *_ecv_array _ecv_invariant(exists i::0.._ecv_value.upb :- _ecv_value[i] == 0) c_string;
 
 #endif /* SRC_GENERAL_ECV_RRF_H_ */
