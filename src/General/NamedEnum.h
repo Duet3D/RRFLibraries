ce7bea1d9eeb0cb7c7b11fbd549c625257609085
David Crocker <dcrocker@eschertech.com>
2025-05-27 13:23:03 +0100

    Resolved an Eclipse symbol shadowing warning

--- src/General/NamedEnum.h
+++ src/General/NamedEnum.h
@@ -126,7 +126,7 @@ public: \
 	constexpr BaseType ToBaseType() const noexcept { return static_cast<BaseType>(v); }							/* convert to integral base type */ \
 	static constexpr BaseType ToBaseType(RawType arg) noexcept { return static_cast<BaseType>(arg); }			/* convert to integral base type */ \
 	c_string ToString() const noexcept { return ToString(v); }													/* conversion to C string */ \
-	static c_string ToString(RawType v) noexcept { return ((BaseType)v < NumValues) ? SkipLeadingUnderscore(_names[(BaseType)v]) : "invalid"; }	/* conversion to C string */ \
+	static c_string ToString(RawType _v) noexcept { return ((BaseType)_v < NumValues) ? SkipLeadingUnderscore(_names[(BaseType)_v]) : "invalid"; }	/* conversion to C string */ \
 	void Assign(BaseType arg) noexcept { v = static_cast<RawType>(arg); }										/* assignment from integral base type */ \
 	bool IsValid() const noexcept { return (BaseType)v < NumValues; }											/* check validity */ \
 private: \
